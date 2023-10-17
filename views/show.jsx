0f7401a202a188c13fe02d3e67d8219c86c3cc27
Johnson Yang <johnsonyang65@gmail.com>
2023-10-16 18:43:50 -0700

    delete route complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
       return (
         <Default>
           <h2>Show Page</h2>
           <h3>{bread.name}</h3>
+          <form action={`/breads/${index}?_method=DELETE`} method="POST">
+             <input type='submit' value="DELETE"/>
+          </form>
           <p>
             and it {
                 bread.hasGluten
