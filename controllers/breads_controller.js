312be34f16e68fcacbd35d783163dcfb0c312087
Johnson Yang <johnsonyang65@gmail.com>
2023-10-16 20:31:59 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,10 +11,17 @@ breads.get('/', (req, res) => {
     )
 })
 
-
 // NEW
 breads.get('/new', (req, res) => {
   res.render('new')
+})
+
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
 })
 
 
@@ -49,6 +56,18 @@ breads.post('/', (req, res) => {
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 
