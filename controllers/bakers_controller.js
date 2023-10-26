5a0d2652df9bbe0462c0c9e0b0ca086a8bd0640e
Johnson Yang <johnsonyang65@gmail.com>
2023-10-25 21:17:30 -0700

    added baker index route

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -5,6 +5,16 @@ const Baker = require('../models/baker.js')
 const bakerSeedData = require('../models/baker_seed.js')
 
 
+// Index: 
+baker.get('/', (req, res) => {
+    Baker.find()
+        .populate('breads')
+        .then(foundBakers => {
+            res.send(foundBakers)
+        })
+})                    
+
+
 // BAKER SEED DATA ROUTE
 baker.get('/data/seed', (req, res) => {
     Baker.insertMany(bakerSeedData).then(res.redirect('/breads'))
